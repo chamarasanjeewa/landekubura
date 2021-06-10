f60ae271e142cb6fc9339aef33c54bc4de9c93bf
chamara sanjeewa <chamara.sanjeewa@gmail.com>
2021-06-10 22:07:47 +0530

    added firebase

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+import fireStore from "./../../lib/firebase";
+
+export default async (req, res) => {
+  const allProducts = await fireStore.collection("products").get();
+  const mappedProducts = await allProducts.docs.map(toProduct);
+  const [first] = mappedProducts;
+  res.statusCode = 200;
+  res.json({ name: first?.name });
+};
+
+const toProduct = doc => {
+  const data = doc.data();
+
+  return {
+    name: data.name
+  };
+};
+
+
