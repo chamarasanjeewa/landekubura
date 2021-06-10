41cb63f838a4ae27fabee8b8548d58de9b1a5846
chamara sanjeewa <chamara.sanjeewa@gmail.com>
2021-06-10 22:20:09 +0530

    more changes

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,20 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import fireStore from "./../../lib/firebase";
+//import fireStore from "./../../lib/firebase";
 
 export default async (req, res) => {
-  const allProducts = await fireStore.collection("products").get();
-  const mappedProducts = await allProducts.docs.map(toProduct);
-  const [first] = mappedProducts;
-  res.statusCode = 200;
-  res.json({ name: first?.name });
-};
+    //   const allProducts = await fireStore.collection("products").get();
+    //   const mappedProducts = await allProducts.docs.map(toProduct);
+    //   const [first] = mappedProducts;
+    //   res.statusCode = 200;
+    //   res.json({ name: first?.name });
+    // };
 
-const toProduct = doc => {
+    // const toProduct = doc => {
   const data = doc.data();
 
   return {
-    name: data.name
+    name:   " data.name"
   };
 };
 
